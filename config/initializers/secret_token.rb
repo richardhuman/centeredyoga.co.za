226f21413d37ad1daf785d42cd2f0cca2aaf95f5
Richard Human <richard@unionswiss.com>
2011-08-22 18:23:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CenterdyogaCoZa::Application.config.secret_token = 'c656a29191bb6adf5b6cc1dbd6da97762c911fc415b82aad40ec92324a081ee423fe66aa846eeaeb17ebeda38f73e322997b008c11e944a94f6d40d5fd3c8500'
