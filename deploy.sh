fd31b88c2ef99e87af873b4fd965079bb29c3a42
Richard Human <richard@unionswiss.com>
2011-08-29 20:11:59 +0200

    Copy changes
    Rounded corners!

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,5 @@ if [ "$1" == "all" ]; then
     ncftpput -u ${USER} -p ${PASSWORD} -R ${HOST} httpdocs ./public/*
 else
     ncftpput -u ${USER} -p ${PASSWORD} ${HOST} httpdocs ./public/*html
+    ncftpput -u ${USER} -p ${PASSWORD} ${HOST} httpdocs/stylesheets ./public/stylesheets/cached-main.css
 fi
