735472a9e3d17312d5e76ccc67598412a7060092
Richard Human <rhuman@gmail.com>
2013-11-09 17:43:27 +0200

    Setting up capistrano

--- Gemfile
+++ Gemfile
@@ -1,5 +1,13 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.0.13'
-gem 'sass'
-#gem 'exception_notification_rails3', '1.2.0', :require => 'exception_notifier'     
+gem 'rails', '~>4.0.0'
+gem 'sass-rails', '~>4.0.0'
+gem 'uglifier', '>=1.3.0'
+gem 'actionpack-page_caching', '~>1.0.0'
+
+gem 'therubyracer', :platforms => :ruby
+
+gem 'capistrano', '~> 3.0', require: false, group: :development
+gem 'capistrano-rails',  require: false, group: :development
+gem 'capistrano-bundler',  require: false, group: :development
+gem 'capistrano-rbenv', github: 'capistrano/rbenv'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,21 +6,37 @@ CenterdyogaCoZa::Application.configure do
   # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  config.eager_load = false    
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+  config.log_level = :debug
+  config.log_tags = [ :remote_ip ] #:uuid
+
+  
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = false
+
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #  :address => 'localhost',
+  #  :port => 1025,
+  #  :domain => 'bio-oil.com',
+  #  :enable_starttls_auto => false
+  #}
+
+
+  #config.action_mailer.delivery_method = :file
+  #config.action_mailer.file_settings = {:location => "#{Rails.root}/tmp/mails"}
+
 end
 
